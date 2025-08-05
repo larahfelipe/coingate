f7f4929adde8ac1268838a70dbdd9fe0fc9835e7
Felipe Lara <felipeahlara@gmail.com>
2025-08-05 00:13:50 -0300

    chore: typo in metadata title fixed and viewport obj added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { AppHeader } from '@/components/app-header';
 import { AppProvider } from '@/providers/app-provider';
 import { Children } from '@/types';
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Poppins } from 'next/font/google';
 import './globals.css';
 
@@ -12,9 +12,16 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: 'coingate | real-time cryptocurrency data & market nsights',
+  title: 'coingate | real-time cryptocurrency data & market insights',
   description:
     'coingate provides real-time crypto market data, in-depth charts, and comprehensive analytics to help you make informed investment decisions. Track thousands of cryptocurrencies with up-to-the-minute insights and performance metrics.',
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  themeColor: '#000000',
+  colorScheme: 'dark',
 };
 
 export default function RootLayout({ children }: Children) {
